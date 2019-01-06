e811f82960ca9b80b9aa7c27144a7bb51ad46a37
Andriy Sorochan <xai1983kbu@gmail.com>
2019-01-06 10:50:08 +0200

    https://www.udemy.com/universal-react-with-nextjs-the-ultimate-guide/learn/v4/content   Project 3 - Cookie Authentication with Express Author - Reed Barger/https://github.com/reedbarger

---
+++ components/LoginForm.js
@@ -0,0 +1,62 @@
+import { loginUser } from '../lib/auth';
+import Router from 'next/router';
+
+class LoginForm extends React.Component {
+    state = {
+        email: "Sincere@april.biz",
+        password: "hildegard.org",
+        error: '',
+        isLoading: false
+    };
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    handleSubmit = event => {
+        const { email, password } = this.state;
+
+        event.preventDefault();
+        this.setState({ error: '', isLoading: true });
+        loginUser(email, password)
+            .then(() => {
+                Router.push("/profile");
+            })
+            .catch(this.showError);
+    }
+
+    showError = err => {
+        console.error(err);
+        const error = err.response && err.response.data || err.message;
+        this.setState({ error, isLoading: false });
+    }
+
+    render() {
+        const { email, password, error, isLoading } = this.state;
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div><input
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    value={email}
+                    onChange={this.handleChange}
+                /></div>
+                <div><input
+                    type="password"
+                    name="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={this.handleChange}
+                /></div>
+                <button disabled={isLoading} type="submit">
+                    {isLoading ? "Sending" : "Submit"}
+                </button>
+                {error && <div>{error}</div>}
+            </form>
+        )
+    }
+}
+
+export default LoginForm;
